9e09198d1f756c24cc3081233b7adda01eb4a77f
aleksandr <aleksandr1104nk@gmail.com>
2019-12-15 22:52:17 +0200

    train 3

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>step by step</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    step by step
                 </div>
 
                 <div class="links">
